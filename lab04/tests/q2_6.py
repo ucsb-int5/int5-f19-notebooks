e5188ab25756fe2daa429b9679645efb9ac70125
YKK <ykk+github@ucsb.edu>
2019-10-30 22:34:27 +0000

    Added Lab04

---
+++ lab04/tests/q2_6.py
@@ -0,0 +1,22 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> get_kth_top_movie_year(4) == 2009
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
